f897ccb4c8450bfd47bf22bf40464447c547c661
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-24 23:52:26 +0530

    [sai] Entering Registration contact list

---
+++ UserRegistration.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.useregistration;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Entering Registration Contact List");
+    }
+
+}
