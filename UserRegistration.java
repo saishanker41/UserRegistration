d3d1b5a3fbe071357b658e2552deb62d4500c5be
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-25 01:05:11 +0530

    [sai] Added Lastname

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,23 +10,32 @@ public class UserRegistration {
     private static final Scanner scan = new Scanner(System.in);
 
     //method
-    public boolean validateUserData(String firstName) {
+    public boolean validateUserData() {
 
-
+        System.out.println("Enter Your First name");
+        String firstName = scan.next();
         System.out.println("The first name is: " + firstName);
         String regExp = "^([A-Z]{1})([a-z]{2,})$";
         Pattern pattern = Pattern.compile(regExp);
         Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
+    }
 
+    public boolean validateUserDataLastName() {
 
-
+        System.out.println("Enter Your Last name");
+        String lastName = scan.next();
+        System.out.println("The last name is: " + lastName);
+        String regExp = "^([A-Z]{1})([a-z]{2,})$";
+        Pattern pattern = Pattern.compile(regExp);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
     }
     public static void main(String[] args) {
-        System.out.println("Enter Your First name");
-        String firstName = scan.next();
-        boolean result = new UserRegistration().validateUserData(firstName);
-        System.out.println(result);
+
+        boolean result1 = new UserRegistration().validateUserData();
+        boolean result2 = new UserRegistration().validateUserDataLastName();
+        System.out.println(result2);
     }
 
 
