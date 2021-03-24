12f50d25caf2894e19f85d7b406d971c15d77546
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-25 01:29:51 +0530

    [sai] Added email format

--- UserRegistration.java
+++ UserRegistration.java
@@ -31,11 +31,23 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(lastName);
         return matcher.matches();
     }
+
+    public boolean validateEmail() {
+        System.out.println("Enter Your Email");
+        String email = scan.next();
+        System.out.println("The email is: " + email);
+        String regExp = "^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-z0-9]+.[a-zA-Z]{2}([.][a-zA-Z]{2,4}){0,1}$";
+        Pattern pattern = Pattern.compile(regExp);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
     public static void main(String[] args) {
 
         boolean result1 = new UserRegistration().validateUserData();
         boolean result2 = new UserRegistration().validateUserDataLastName();
-        System.out.println(result2);
+        boolean result3 = new UserRegistration().validateEmail();
+        System.out.println(result3);
     }
 
 
