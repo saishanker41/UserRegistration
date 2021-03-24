aae6df993b6d3443bb66194f4ecea8899bfeacd5
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-25 00:48:07 +0530

    [sai] Entered valid firstname condition

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,8 +1,34 @@
 package com.bridgelabz.useregistration;
 
+
+import java.util.Scanner;
+import java.lang.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Entering Registration Contact List");
+    private static final Scanner scan = new Scanner(System.in);
+
+    //method
+    public boolean validateUserData(String firstName) {
+
+
+        System.out.println("The first name is: " + firstName);
+        String regExp = "^([A-Z]{1})([a-z]{2,})$";
+        Pattern pattern = Pattern.compile(regExp);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+
+
+
     }
+    public static void main(String[] args) {
+        System.out.println("Enter Your First name");
+        String firstName = scan.next();
+        boolean result = new UserRegistration().validateUserData(firstName);
+        System.out.println(result);
+    }
+
+
 
 }
